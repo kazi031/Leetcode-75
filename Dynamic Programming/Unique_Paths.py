2050c1f5e833b25f6906e8864bb55944924f5969
kazi031 <kazirafidraiyan031@gmail.com>
2023-03-01 22:39:25 +0600

    Day11 P2

---
+++ Dynamic Programming/Unique_Paths.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+        for i in range(m-1):
+            new_row = [1] * n
+            for j in range(n-2, -1, -1):
+                new_row[j] = new_row[j+1] + row[j]
+            row = new_row
+        return row[0]
