0048cb2672e7cd9b246f7ae64e89387acdcd3bf2
kazi031 <kazirafidraiyan031@gmail.com>
2023-03-11 14:32:33 +0600

    Day14 P2 Solved

--- Heap/Top_K_Freq_Words.py
+++ Heap/Top_K_Freq_Words.py
@@ -14,13 +14,21 @@ class Solution:
         for n,c in hashMap.items():
             freq[c].append(n)
         #print(SortedhashMap)
-        #print(freq)
+        print(freq)
         outString = []
         for i in range(len(freq)-1, 0, -1):
-            if freq[i]:
+            if len(freq[i])>0:
                 newList = freq[i]
-                heapq.heapify(newList)
+                print(newList)
+                #heapq.heapify(newList)
+                newList.sort()
+                print(newList)
                 for n in newList:
                     outString.append(n)
-                if len(outString) == k:
+                    #print(outString)
+                if len(outString) > k:
+                    while len(outString) != k:
+                        outString.pop()
+                    return outString
+                elif len(outString) == k:
                     return outString
