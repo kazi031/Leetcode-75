b0d8056439c6129fb5a18d4ab75ef0e598fc9cdd
kazi031 <kazirafidraiyan031@gmail.com>
2023-03-22 12:30:35 +0600

    Day15 P2

---
+++ Implementation&Simulation/SpiralMatrix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        top, bottom = 0, len(matrix)
+        left, right = 0, len(matrix[0])
+
+        while left < right and top < bottom:
+            #1st get every i in the top row
+            for i in range(left,right):
+                res.append(matrix[top][i])
+            top += 1
+
+            #get every i in the right most column
+            for i in range(top, bottom):
+                res.append(matrix[i][right-1])
+            right -= 1
+
+            if not (left < right and top < bottom):
+                break
